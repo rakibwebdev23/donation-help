ec3220e8256b3d31b3bf36df8369da16e9ebccdb
Rakib <rh.rakibhasan365@gmail.com>
2025-01-31 18:52:36 +0600

    about section update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,27 @@
+import Container from "../Container/Container";
+import img from "../../assets/images/helpHand/about.jpg";
+import { Link } from "react-router-dom";
+
 const About = () => {
     return (
-        <div className="bg-slate-300 text-black p-10 rounded-md m-10">
-            <h5 className="p-10 text-center text-xl">We helped the people from all over the world raise millions for causes, special events, projects and so much more.</h5>
+        <div>
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage: `url(${img})`,
+                }}>
+                <div className="hero-overlay bg-black bg-opacity-50"></div>
+                <div className="hero-content text-neutral-content text-center ">
+                    <div className="max-w-screen-md">
+                        <h1 className="mb-5 lg:text-6xl text-2xl font-bold uppercase">About Us</h1>
+                        <div className="text-xl font-semibold">
+                            <Link className="hover:text-rose-600" to="/">Home <span className="ml-2 mr-2 text-rose-600 text-xl font-bold">|</span></Link>
+                            <Link className="ml-2 hover:text-rose-600" to="/needhelp">Need Help</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Container></Container>
         </div>
     );
 };
